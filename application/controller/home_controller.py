94586e7af65b899441b0372e1384a2f2f3cc39c0
Pedro Henrique Vasconcelos <pedroh.cpv@hotmail.com>
2020-12-22 23:03:40 -0300

    Controller
    Foi feito o Controller para exibição do projeto

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -1,7 +1,11 @@
 from application import app
 from flask import render_template, request
+import json
+from application.model.entity.produto import Produto
+from application.model.dao.produto_DAO import ProdutoDAO
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    todos_produtos = ProdutoDAO().get_exibir_produtos()
+    return render_template("index.html", todos_produtos = todos_produtos)
